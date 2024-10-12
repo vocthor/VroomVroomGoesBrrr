e6ef50e97109455328fc0fb8d8ba77af7a548aa1
Jeremy.Augereau <augereau.jeremy@protonmail.com>
2024-10-13 00:11:28 +0200

    adding routing and default routes

--- src/controllers/web.rs
+++ src/controllers/web.rs
@@ -3,10 +3,11 @@ use axum::{
     http::StatusCode,
     Json, Router,
 };
-
+use axum::extract::Path;
+use crate::controllers::models::ServerInfo;
 
 pub fn start_web_server(port: u16) {
-    tokio::spawn(async move{
+    tokio::spawn(async move {
         start(port).await;
     });
 }
@@ -15,12 +16,38 @@ async fn start(port: u16) {
     tracing_subscriber::fmt::init();
 
     let app = Router::new()
-        .route("/", get(hello_handler));
+        .route("/", get(hello_handler))
+        .route("/server/start", post(start_trackmania_server))
+        .route("/server/stop", post(stop_trackmania_server))
+        .route("/server/list", get(list_trackmania_servers))
+        .route("/server/{id}", get(get_trackmania_server_info));
+
 
     let addr = format!("localhost:{}", port);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn start_trackmania_server() -> (StatusCode, Json<&'static str>) {
+    (StatusCode::CREATED, Json("Server started"))
+}
+async fn stop_trackmania_server() -> StatusCode {
+    StatusCode::ACCEPTED
+}
+async fn list_trackmania_servers() -> (StatusCode, Json<Vec<ServerInfo>>) {
+    (StatusCode::OK,
+     Json(
+         vec![
+             ServerInfo::new(1),
+             ServerInfo::new(2),
+             ServerInfo::new(3),
+         ]
+     )
+    )
+}
+async fn get_trackmania_server_info(Path(id): Path<u32>) -> (StatusCode,Json<ServerInfo>) {
+    (StatusCode::OK, Json(ServerInfo::new(id)))
 }
 
 async fn hello_handler() -> &'static str {
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use std::thread::Thread;
-use crate::controllers::web::start_web_server;
-
 mod controllers;
 
+use crate::controllers::web::start_web_server;
+
 #[tokio::main]
+/// The main entry point of the application.
 async fn main() {
     start_web_server(3000);
+
     loop {
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
     }
 }
-
+/// Stops the server and exits the process.
 pub fn stop(){
     println!("Server stopped");
     std::process::exit(0);
